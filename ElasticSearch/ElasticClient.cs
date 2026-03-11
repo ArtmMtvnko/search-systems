89950881de3fddca882fa820a19940981e94722b
ArtmMtvnko <artemm3note@gmail.com>
2026-03-11 23:57:25 +0200

    Refactor search to support multi-field and move query logic
    Refactored search functionality to allow users to select and search across multiple fields simultaneously. Moved ElasticSearch query-building logic from ElasticClient to a new ElasticQueryBuilder class, improving code organization and maintainability. Removed single-field and "All" search options in favor of flexible multi-field selection.

--- ElasticSearch/ElasticClient.cs
+++ ElasticSearch/ElasticClient.cs
@@ -33,95 +33,17 @@ class ElasticClient
         _indexName = configuration["IndexName"] ?? throw new KeyNotFoundException("IndexName is not provided");
     }
 
-    public Task<List<ElasticSearchHit>> GetAllDocumentsAsync() => SearchAsync(new
-    {
-        query = new
-        {
-            match_all = new { }
-        }
-    });
+    public Task<List<ElasticSearchHit>> GetAllDocumentsAsync() => SearchAsync(ElasticQueryBuilder.CreateMatchAllQuery());
 
     public Task<HttpResponseMessage> InsertDocumentAsync(ElasticDocument doc) => PostJsonAsync($"{_indexName}/_doc", doc);
 
     public Task<HttpResponseMessage> RemoveDocumentAsync(string id) => _httpClient.DeleteAsync($"{_indexName}/_doc/{id}");
 
-    public Task<List<ElasticSearchHit>> SearchByTextAsync(string field, string query)
-    {
-        object payload = field == "all"
-            ? new
-            {
-                query = new
-                {
-                    multi_match = new
-                    {
-                        query,
-                        fields = new[] { "description", "history", "code_example" }
-                    }
-                }
-            }
-            : new
-            {
-                query = new
-                {
-                    match = new Dictionary<string, string>
-                    {
-                        [field] = query
-                    }
-                }
-            };
+    public Task<List<ElasticSearchHit>> SearchByTextAsync(IEnumerable<string> fields, string query) =>
+        SearchAsync(ElasticQueryBuilder.CreateTextSearchQuery(fields, query));
 
-        return SearchAsync(payload);
-    }
-
-    public Task<List<ElasticSearchHit>> FilterAsync(FilterParams filterParams)
-    {
-        var filters = BuildFilters(filterParams);
-
-        var query = new
-        {
-            query = new
-            {
-                constant_score = new
-                {
-                    filter = new
-                    {
-                        @bool = new
-                        {
-                            filter = filters
-                        }
-                    }
-                }
-            }
-        };
-
-        return SearchAsync(query);
-    }
-
-    private static List<object> BuildFilters(FilterParams filterParams)
-    {
-        var filters = new List<object>
-        {
-            CreateRangeFilter("first_appeared", filterParams.FirstAppearedAfter, filterParams.FirstAppearedBefore),
-            CreateRangeFilter("active_users", filterParams.ActiveUsersMoreThan, filterParams.ActiveUsersLessThan)
-        };
-
-        if (!string.IsNullOrWhiteSpace(filterParams.Name))
-        {
-            filters.Add(CreateWildcardFilter("name", filterParams.Name));
-        }
-
-        if (filterParams.IsStaticallyTyped is not null)
-        {
-            filters.Add(CreateTermFilter("is_statically_typed", filterParams.IsStaticallyTyped.Value));
-        }
-
-        if (filterParams.Paradigms.Count > 0)
-        {
-            filters.Add(CreateTermsFilter("paradigms", filterParams.Paradigms));
-        }
-
-        return filters;
-    }
+    public Task<List<ElasticSearchHit>> FilterAsync(FilterParams filterParams) =>
+        SearchAsync(ElasticQueryBuilder.CreateFilterQuery(filterParams));
 
     private async Task<List<ElasticSearchHit>> SearchAsync(object payload)
     {
@@ -135,46 +57,6 @@ class ElasticClient
     private Task<HttpResponseMessage> PostJsonAsync(string requestUri, object payload) => 
         _httpClient.PostAsJsonAsync(requestUri, payload, SerializerOptions);
     
-
-    private static object CreateRangeFilter<T>(string field, T? gte, T lte) => new
-    {
-        range = new Dictionary<string, object>
-        {
-            [field] = new
-            {
-                gte,
-                lte
-            }
-        }
-    };
-
-    private static object CreateTermsFilter(string field, IEnumerable<string> values) => new
-    {
-        terms = new Dictionary<string, IEnumerable<string>>
-        {
-            [field] = values
-        }
-    };
-
-    private static object CreateWildcardFilter(string field, string value) => new
-    {
-        wildcard = new Dictionary<string, object>
-        {
-            [field] = new
-            {
-                value,
-                case_insensitive = true
-            }
-        }
-    };
-
-    private static object CreateTermFilter(string field, bool value) => new
-    {
-        term = new Dictionary<string, object>
-        {
-            [field] = value
-        }
-    };
 
     private static async Task<ElasticSearchResponse> ParseElasticHitsAsync(HttpResponseMessage response)
     {
