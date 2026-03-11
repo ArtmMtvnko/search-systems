935e0ab3038ab2674bb5f7ad33509864f9087143
ArtmMtvnko <artemm3note@gmail.com>
2026-03-12 00:10:50 +0200

    Add score fields to search response and hits
    Added MaxScore to ElasticSearchHits and Score to ElasticSearchHit, both mapped to their respective JSON fields. Updated CreateEmptySearchResponse to initialize MaxScore to 0.

--- ElasticSearch/Models/ElasticSearchResponse.cs
+++ ElasticSearch/Models/ElasticSearchResponse.cs
@@ -12,12 +12,18 @@ class ElasticSearchHits
 {
     [JsonPropertyName("hits")]
     public required List<ElasticSearchHit> Documents { get; set; }
+
+    [JsonPropertyName("max_score")]
+    public required double MaxScore { get; set; }
 }
 
 class ElasticSearchHit
 {
     [JsonPropertyName("_id")]
     public required string Id { get; set; }
+
+    [JsonPropertyName("_score")]
+    public required double Score { get; set; }
 
     [JsonPropertyName("_source")]
     public required ElasticDocument ElasticDocument { get; set; }
